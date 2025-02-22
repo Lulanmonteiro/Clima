902f52945009d79ad0fca3c374c5f1f0d36d5a1f
Lulanmonteiro <110698165+Lulanmonteiro@users.noreply.github.com>
2025-02-21 22:11:02 -0300

    script.js

--- static/script.js
+++ static/script.js
@@ -5,22 +5,25 @@ function buscarClima() {
         return;
     }
 
-    fetch(`/clima?cidade=${cidade}`)
+    const apiKey = "506a80532eade5d8241b2807f8da4dd3"; 
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+    fetch(url)
         .then(response => response.json())
         .then(data => {
-            if (data.erro) {
+            if (data.cod === "404") {
                 alert("Cidade não encontrada!");
                 return;
             }
 
-            document.getElementById("cidade-nome").textContent = data.cidade;
-            document.getElementById("descricao").textContent = data.descricao;
-            document.getElementById("icone").src = data.icone;
-            document.getElementById("temperatura").textContent = data.temperatura;
-            document.getElementById("vento").textContent = data.vento;
-            document.getElementById("umidade").textContent = data.umidade;
-            document.getElementById("sensacao").textContent = data.sensacao_termica;
-            document.getElementById("pressao").textContent = data.pressao;
+            document.getElementById("cidade-nome").textContent = data.name;
+            document.getElementById("descricao").textContent = data.weather[0].description;
+            document.getElementById("icone").src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+            document.getElementById("temperatura").textContent = data.main.temp;
+            document.getElementById("vento").textContent = data.wind.speed;
+            document.getElementById("umidade").textContent = data.main.humidity;
+            document.getElementById("sensacao").textContent = data.main.feels_like;
+            document.getElementById("pressao").textContent = data.main.pressure;
 
             document.getElementById("resultado").classList.remove("hidden");
         })
